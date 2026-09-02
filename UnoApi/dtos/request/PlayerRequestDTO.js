e1d2b504bbf284288f5cadf3300818d8e4c40e64
Enio Martins <enio.martins0125@jala.university>
2026-09-02 06:15:57 -0300

    Remove player request DTO

--- UnoApi/dtos/request/PlayerRequestDTO.js
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod';
-
-export const createPlayerSchema = z.object({
-  name: z.string().min(2, 'Name must be at least 2 characters long'),
-  email: z.string().email('Invalid email'),
-  age: z.number().int().min(0, 'Age must be a positive number').max(120, 'Age must be 120 or less')
-});
-
-export const updatePlayerSchema = createPlayerSchema.partial();
