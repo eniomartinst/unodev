7b83d5a191aebe78354b919a284b86b886e1ef90
Enio Martins <eniomt1.0@gmail.com>
2026-09-03 04:59:39 -0300

    Add score request dto

---
+++ UnoApi/dtos/request/ScoreRequestDTO.js
@@ -0,0 +1,8 @@
+import { z } from 'zod';
+export const createScoreSchema = z.object({
+  playerId: z.coerce.number().int().positive(),
+  gameId: z.coerce.number().int().positive(),
+  score: z.coerce.number().int()
+});
+
+export const updateScoreSchema = createScoreSchema.partial();
